6ccbc21ec602e0173a6cf63fa9858c73b8ac5f7a
lucasmvaladao <contato.valadao14@gmail.com>
2024-04-17 11:33:45 -0300

    Add files via upload

---
+++ src/componentes/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./estilo.css";
+
+function Header(){
+    return(
+        <header>
+            <p>Projeto Criação de API</p>
+        </header>
+    )
+};
+export default Header;
