ee490b24436395542067385945b11faa8836eaff
lucasmvaladao <contato.valadao14@gmail.com>
2024-04-17 11:52:20 -0300

    Add files via upload

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -1,10 +1,19 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import "./estilo.css";
 
 function Header(){
     return(
         <header>
-            <p>Projeto Criação de API</p>
+            <div>
+                <p>Projeto Criação de API</p>
+                
+           </div>
+            <div>
+           <Link className="topo" to="/">
+            <img src="" alt="logoSenai"/>    
+            </Link>
+           </div>
         </header>
     )
 };
