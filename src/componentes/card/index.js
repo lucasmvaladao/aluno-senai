151e952451ac4c4c93fd4ea6a6cf99b29c18593d
valadao14 <contato.valadao14@gmail.com>
2024-04-23 08:17:18 -0300

    arrumando

--- src/componentes/card/index.js
+++ src/componentes/card/index.js
@@ -1,28 +1,59 @@
 import React from "react";
+import user from '../../img/user.png'
 import './card.css'
 
 function Card (){
     return(
         <div className="container">
+            <div className="sla">
             <h1>
                 ALUNOS SENAI-SP
+                <br/>
                 INFORMAÇÕES CADASTRADAS
             </h1>
-            <button className="adicionar"> ADICIONAR  <stron> + </stron> </button>
-
+            <button className="adicionar"> ADICIONAR  <strong> + </strong> </button>
+            </div>
             <section className="sessao">
                 <ul className="lista">
-                    <li> aluno 1 </li>
+                    <li>
+                        <img src={ user } alt="imagem aluno"/>
+                        <h2 className="nome"> ALUNO 01 </h2>
+                        <button className="visu"> VISUALIZAR </button>
+                        <button className="excluir"> EXCLUIR </button>
+                    </li>
 
-                    <li>aluno2</li>
+                    <li>
+                        <img src={ user } alt="imagem aluno"/>
+                        <h2 className="nome"> ALUNO 02 </h2>
+                        <button className="visu"> VISUALIZAR </button>
+                        <button className="excluir"> EXCLUIR </button>
+                    </li>
 
-                    <li>aluno3</li>
+                    <li>
+                        <img src={ user } alt="imagem aluno"/>
+                        <h2 className="nome"> ALUNO 03 </h2>
+                        <button className="visu"> VISUALIZAR </button>
+                        <button className="excluir"> EXCLUIR </button>
+                    </li>
 
-                    <li>aluno4</li>
+                    <li>
+                        <img src={ user } alt="imagem aluno"/>
+                        <h2 className="nome"> ALUNO 04 </h2>
+                        <button className="visu"> VISUALIZAR </button>
+                        <button className="excluir"> EXCLUIR </button>
+                    </li>
 
-                    <li>aluno5</li>
+                    <li>
+                        <img src={ user } alt="imagem aluno"/>
+                        <h2 className="nome"> ALUNO 05 </h2>
+                        <button className="visu"> VISUALIZAR </button>
+                        <button className="excluir"> EXCLUIR </button>
+                    </li>
+
+                    
                 </ul>
             </section>
+            <div className="rodape"></div>
         </div>
     )
 }
